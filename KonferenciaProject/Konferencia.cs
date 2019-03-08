efb88688f605939d59d672e7192d970543cf7870
Petrik Lajos Szg <info@petrik.hu>
2019-03-08 16:38:38 +0100

    órai munka

---
+++ KonferenciaProject/Konferencia.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonferenciaProject
+{
+    class Konferencia
+    {
+        private List<Eloadas> eloadasok;
+
+        public Konferencia(List<Eloadas> eloadasok)
+        {
+            this.Eloadasok = eloadasok;
+
+            try {
+                StreamReader sr = new StreamReader("konferencia.txt");
+                string s = sr.ReadLine();
+                string[] szetszedes = s.Split(';');
+
+                for (int i = 0; i < eloadasok.Count; i++)
+                {
+
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine("Hiba: " + e);
+            }
+        }
+
+        internal List<Eloadas> Eloadasok
+        {
+            get
+            {
+                return eloadasok;
+            }
+
+            set
+            {
+                eloadasok = value;
+            }
+        }
+
+    }
+}
